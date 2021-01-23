daacf47e29c632cc387132567c64854bd858a2ca
Andrey Dvoytsov <annihilator1981@mail.ru>
2021-01-23 17:10:53 +0500

    Added ARKit SDK

--- FaceReality/ViewController.swift
+++ FaceReality/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Andy Dvoytsov on 23.01.2021.
 //
 
-import UIKit
+import ARKit
 import RealityKit
 
 class ViewController: UIViewController {
@@ -15,11 +15,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let configuration = ARFaceTrackingConfiguration() // создаем конфигурацию для распознавания лиц, нужен телефон с TrueDepth-камерой
+        arView.session.run(configuration) // запускаем ARView с созданной конфигурацией
+        // если пользователю поступил звонок или он свернул приложение - надо останавливать AR-сессию, т.к. она тратит много ресурсов
+        
+        
         // загружаем из 3d-файла FaceReal объект
-        let faceAnchor = try! FaceR.loadСцена()
+        let faceAnchor = try! FaceR.loadScene()
         
-        
-        // загружаем объект на сцеун
+        // загружаем объект на сцену
         arView.scene.anchors.append(faceAnchor)
     }
 }
