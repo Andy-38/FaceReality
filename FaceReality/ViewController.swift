d10a88514112cc9c5610b67bc3c0b453874badae
Andrey Dvoytsov <annihilator1981@mail.ru>
2021-01-23 16:15:10 +0500

    Initial Commit

---
+++ FaceReality/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FaceReality
+//
+//  Created by Andy Dvoytsov on 23.01.2021.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
